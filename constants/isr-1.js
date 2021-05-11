6d0059379490088159f5519496bd70b4ff1c735f
callmenick <nick@callmenick.com>
2021-05-11 16:58:20 -0400

    Adds more consts

--- constants/isr-1.js
+++ constants/isr-1.js
@@ -9,16 +9,3 @@ export const code = `export async function getStaticProps() {
     revalidate: 2,
   }
 }`;
-
-export const localResults = `isr-1 getStaticProps running, count: 1
-isr-1 getStaticProps running, count: 2
-isr-1 getStaticProps running, count: 3
-isr-1 getStaticProps running, count: 4
-isr-1 getStaticProps running, count: 5
-isr-1 getStaticProps running, count: 6
-isr-1 getStaticProps running, count: 7
-isr-1 getStaticProps running, count: 8
-isr-1 getStaticProps running, count: 9
-`;
-
-export const vercelResults = ``;
