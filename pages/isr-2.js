41d74fb9354d45dc64eff2559db3add3daa474aa
callmenick <nick@callmenick.com>
2021-05-11 15:33:13 -0400

    Switches to random api

--- pages/isr-2.js
+++ pages/isr-2.js
@@ -3,9 +3,7 @@ import Head from 'next/head'
 import Prism from 'prismjs';
 import { code, localResults, vercelResults } from '../constants/isr-2';
 
-let count = 0;
-
-function Isr2({ count }) {
+function Isr2({ num }) {
   useEffect(() => {
     Prism.highlightAll();
   }, []);
@@ -13,40 +11,39 @@ function Isr2({ count }) {
   return (
     <div>
       <h1>ISR Test #2</h1>
-      <p>A simple counter based test. When the count hits 5, then getStaticProps throws an error that is not caught.</p>
-      <h2>Count</h2>
-      <p>{count}</p>
+      <p>A simple number based test. Fetches a random number from an API. If it's {`<=`} 5, throws an error.</p>
+      <h2>Num</h2>
+      <p>{num}</p>
       <h2>Code</h2>
       <pre>
         <code className="language-javascript">{code}</code>
       </pre>
       <h2>Console</h2>
       <h3>Local production build:</h3>
-      <pre>
-        <code className="language-shell">{localResults}</code>
-      </pre>
+      <p>Shows the console message as outlined in the code. Logs the error when there is an error.</p>
       <h3>Vercel deployments:</h3>
-      <p>getStaticProps runs and the count goes up in order. But when it hits count number 5, it errors, and then re-runs validation again automatically after what seems like the revalidation period of n seconds. However it doesn't continue incrementing from where it left off… it's almost like it bounces to a new instance and starts the whole thing over from count = 1.</p>
-      <pre>
-        <code className="language-shell">{vercelResults}</code>
-      </pre>
+      <p></p>
       <h2>Visual</h2>
       <h3>Local production build:</h3>
-      <p>No hiccups. User always sees the page and count variable. Even on the try that errors, the page will not break.</p>
+      <p>No hiccups. User always sees the page and num variable. Even on the try that errors, the page will not break.</p>
       <h3>Vercel deployments:</h3>
-      <p>Same as above, but user sees count going from 1 to 4, then back to 1.</p>
+      <p>Same as above, but user sees num going from 1 to 4, then back to 1.</p>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  count += 1;
-  console.log('isr-2 getStaticProps running, count:', count);
-  if (count === 5) throw new Error('Failed to get static props');
+  const res = await fetch('https://random-data-api.com/api/number/random_number');
+  const data = await res.json();
+  const num = data.digit;
+
+  console.log('isr-2 getStaticProps running, num:', num);
+
+  if (num <= 5) throw new Error('Failed to get static props');
 
   return {
     props: {
-      count,
+      num,
     },
     revalidate: 2,
   }
